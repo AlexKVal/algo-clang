755abe534dfc1c5b2eed1564ed9e7412ccc17a58
Alexander Shemetovskiy <alexkval@gmail.com>
2019-04-07 14:56:41 +0100

    Implement selection sort without using a subarray

--- selection-sort.c
+++ selection-sort.c
@@ -3,33 +3,19 @@
 #include "util.h"
 
 void selection_sort(int arr[], size_t arr_size) {
-  // copy the unsorted array into a subarray and work from there
-  int unsorted_arr[arr_size];
-  memcpy(unsorted_arr, arr, arr_size * sizeof(int));
-
-  size_t sorted_idx = 0; // the sorted result will be built from left to right
-
-  int unsorted_size = arr_size;
-
-  while (unsorted_size > 0) {
-    // find the minimum in the unsorted starting from the first element
-    int min = unsorted_arr[0];
-    size_t idx_min = 0;
-    for (size_t i = 0; i < unsorted_size; i++) {
-      if (unsorted_arr[i] < min) {
-        min = unsorted_arr[i];
+  for (size_t start_idx = 0; start_idx < arr_size; start_idx++) {
+    // find the min among the rest of unsorted elements
+    int min = arr[start_idx];
+    size_t idx_min = start_idx;
+    for (size_t i = start_idx; i < arr_size; i++) {
+      if (arr[i] < min) {
+        min = arr[i];
         idx_min = i;
       }
     }
-
-    // remove the min value and shift left the rest of the unsorted array elements
-    unsorted_size--;
-    for (size_t j = idx_min; j < unsorted_size; j++) {
-      unsorted_arr[j] = unsorted_arr[j + 1];
-    }
-
-    arr[sorted_idx] = min;
-    sorted_idx++;
+    // swap the non-minimum value into the unsorted part
+    arr[idx_min] = arr[start_idx];
+    arr[start_idx] = min;
   }
 }
 
